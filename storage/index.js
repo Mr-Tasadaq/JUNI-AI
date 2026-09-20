be405c6d8cc3ee35c38228d99ffdb211bd9d4ecd
JuniAi <alitasadaq44@gmail.com>
2026-09-21 01:43:41 +0300

    feat: expose storage primitives

---
+++ storage/index.js
@@ -0,0 +1,2 @@
+export { createJuniDatabase, resolveDatabaseConfig, queryOne, queryAll } from "./database.js";
+export { canonicalJson, hashObject, hashString, byteSize, toJson, fromJson } from "./serialization.js";
