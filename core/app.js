909581379c6c9191a96c9614c6e05a701389858b
JuniAi <alitasadaq44@gmail.com>
2026-09-21 01:23:02 +0300

    feat: add single application composition root

---
+++ core/app.js
@@ -0,0 +1,35 @@
+import { loadConfig } from "./config.js";
+import { EventBus, createMemoryEventSink } from "./events.js";
+import { buildSystemIdentity } from "./identity.js";
+import { createJuni } from "./juni.js";
+import { createRouter } from "./router.js";
+import { createToolRegistry } from "./tools.js";
+import { createProviderRegistry } from "../providers/index.js";
+import { VoiceSessionController } from "./voice.js";
+
+export function createJuniApplication({ env = process.env, tools = createToolRegistry(), eventSink } = {}) {
+  const config = loadConfig(env);
+  const events = new EventBus({ maxPayloadBytes: config.observability.maxEventPayloadBytes });
+  const sink = eventSink ?? createMemoryEventSink();
+
+  events.subscribe((event) => sink.push(event));
+
+  const providers = createProviderRegistry(config);
+  const router = createRouter({ providers, config, events });
+  const juni = createJuni({ config, router, tools, events });
+  const voice = new VoiceSessionController({
+    sessionFactory: (options) => providers.geminiLive.connect(options),
+    onEvent: (event) => events.emit(event.type, event),
+  });
+
+  return Object.freeze({
+    config,
+    events,
+    sink,
+    providers,
+    router,
+    tools,
+    juni,
+    voice,
+  });
+}
