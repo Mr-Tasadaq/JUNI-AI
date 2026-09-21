e18d985d7437c5370dc479fd1bf40c11f66be4e1
JuniAi <alitasadaq44@gmail.com>
2026-09-21 16:11:03 +0300

    Export voice token contract

--- core/index.js
+++ core/index.js
@@ -8,6 +8,7 @@ export { ProviderError, RouterError, JuniError } from "./errors.js";
 export { authorizeRequest, checkOrigin, redactSecrets, sanitizeEventData, safeTokenEquals } from "./security.js";
 export { createProvenanceRecord, AI_BLOCKCHAIN_BOUNDARY } from "./provenance.js";
 export { assertVoiceSessionContract, VoiceSessionController, VOICE_EVENTS } from "./voice.js";
+export { createGeminiEphemeralToken } from "./voice-token.js";
 export { createJuniMemoryApplication } from "../memory/app.js";
 
 export { createJuniResearchApplication } from "../research/app.js";
