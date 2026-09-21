7ec378434d8b6ce7caa7a4534d3871ecef132f02
JuniAi <alitasadaq44@gmail.com>
2026-09-21 04:03:40 +0300

    Step 3: advance storage schema version

--- storage/database.js
+++ storage/database.js
@@ -1,7 +1,7 @@
 import { readFile } from "node:fs/promises";
 import { createClient } from "@libsql/client";
 
-const SCHEMA_VERSION = 1;
+const SCHEMA_VERSION = 2;
 
 function splitStatements(sql) {
   return sql
