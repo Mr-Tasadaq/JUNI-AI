47035947e274a52b4a0fe75b3f4271e3de9eb572
JuniAi <alitasadaq44@gmail.com>
2026-09-21 04:03:25 +0300

    Step 3: add research evidence, ranking, citations, synthesis, and generic adapter

---
+++ research/index.js
@@ -0,0 +1,6 @@
+export { createJuniResearchApplication } from "./app.js";
+export * from "./model.js";
+export { SafeWebRetriever } from "./url-fetcher.js";
+export { ResearchStorage } from "./storage.js";
+export { createGenericResearchProvider } from "./generic.js";
+export { validateExternalUrl, promptInjectionIndicators } from "./security.js";
